59af5dab61c4c9281ca602370d8a9105b68668d2
Abio-Truong <95080850+Abio-Truong@users.noreply.github.com>
2021-11-26 05:16:18 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -154,10 +154,7 @@ function addSpendingGraph() {
 	                    'Week 1',
 	                    'Week 2',
 	                    'Week 3',
-	                    'Week 4',
-	                    'Week 5',
-	                    'Week 6',
-	                    'Week 7'
+	                    'Week 4'
 	                    ],
 	                datasets: [{
 	                    label: '',
